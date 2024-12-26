9a5a5f635e19ff4547be3af0215ee15a6142bb01
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2024-12-26 19:05:16 +0100

    Add files via upload

---
+++ app/nava.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Title and description
+st.title("AI powered Cowpea Management System")
+st.markdown("""
+This dashboard allows you to navigate to different Ai powered application for Cowpea management system.
+""")
+
+# Navigation links
+st.header("Available Apps")
+st.markdown("""
+- [Cowpea Disease Predictor](https://cowpea-project-23ni8gknzgwryhzuhp7kcs.streamlit.app/)  
+- [Treatment Predictor](https://cowpeatreatment-hyb7fw82sltrlwfmiypyjt.streamlit.app/)  
+- [Yield Predictor](https://cowpeayield-9extd5lp5lptxwtw3bdmqg.streamlit.app/)  
+""")
+
+# Optionally, provide a dropdown for navigation
