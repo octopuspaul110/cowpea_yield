5bbc516cbc691d4617fbac73c2af39942dc5cad2
Adegbite Paul <105382756+octopuspaul110@users.noreply.github.com>
2024-12-27 09:10:33 +0100

    Update nava.py

--- app/nava.py
+++ app/nava.py
@@ -8,7 +8,7 @@ st.title("AI-Powered Integrated Cowpea management system for Nigerian Farmers.")
 dir = path.Path(__file__)
 sys.path.append(dir.parent.parent)
 
-image_path = "./app/cowpea_images.png"  # Replace with the actual path to your image
+image_path = "./app/cowpea_images.jpeg"  # Replace with the actual path to your image
 try:
     image = Image.open(image_path)
     st.image(image, caption="Cowpea Yield", use_column_width=True)
